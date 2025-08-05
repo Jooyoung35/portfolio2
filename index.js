77a75cebff57eea56d0f7c9a49092b5de7e695d4
Jooyoung35 <jykimjulia@gmail.com>
2025-08-05 17:42:36 +0900

    두번째 업로드

--- index.js
+++ index.js
@@ -26,3 +26,60 @@
       markers: false,
       anticipatePin: 1
   });
+
+
+  // 배경고정 (고정시키기)
+  // const panel = document.querySelector("#intro"); //고정할 섹션 넣기
+
+  // ScrollTrigger.create({
+  //     trigger: panel,
+  //     start: "top top",
+  //     pin: true,
+  //     pinSpacing: false
+  // });
+
+
+
+
+  // profile 나타나기
+//   gsap.registerPlugin(ScrollTrigger);
+//   gsap.utils.toArray(".ex-wrap").forEach((item) => {
+//   gsap.set(item, { autoAlpha: 0, x: 100 }); 
+
+//   ScrollTrigger.create({
+//     trigger: item,
+//     start: "top 80%",     
+//     end: "bottom 20%",     
+//     onEnter: () => animate(item),
+//     // markers: false,
+//   });
+// });
+
+// const animate = (item) => {
+//   gsap.to(item, {
+//     autoAlpha: 1,
+//     x: 0,
+//     duration: 1.2,
+//     ease: "expo.out", //부드럽게 나타나기!!!
+//     overwrite: "auto"
+//   });
+// };
+
+
+//info나타나기
+    const ani2 = gsap.timeline();
+    ani2.from(".img-area", {x: -150, autoAlpha:0}) //왼쪽 -200px, opacity 0 시작 > 점점 나타나게
+        .from(".txt-area", {x: 100, autoAlpha:0}) 
+
+    ScrollTrigger.create({
+        animation: ani2,
+        trigger: ".info", //brand-intro가 화면에 들어오면 애니메이션 작동
+        start: "-=700 top", //시작점 기준보다 700px위에
+        end: "+=400",
+        scrub: 1,
+        pin: false,
+        anticipatePin: 1,
+        markers: false,
+    });
+
+
